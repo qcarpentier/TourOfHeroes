bdb03bcf03e0e32cfc9a14543f782d2238e3e2b8
Carpentier Quentin <quentcarp@hotmail.com>
2018-02-07 19:18:26 +0100

    HeroService is now Async and MessageService is implemented

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,17 +1,21 @@
 import { Injectable } from '@angular/core';
-import { Hero } from "./hero";
-import { HEROES } from "./mock-heroes";
 
 // Using RxJS to implement Observable
 import { Observable } from "rxjs/Observable";
-import { of } from "rxjs/Observable/of";
+import { of } from "rxjs/observable/of"
+import { Hero } from "./hero";
+import { HEROES } from "./mock-heroes";
+import { MessageService } from "./message.service";
 
 @Injectable()
 export class HeroService {
 
-  constructor() { }
+  // Service-in-service scenario > inject the MessageService into the HeroService which is injected into the HeroesComponent
+  constructor(private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]>{
+    // TODO: send the message _after_ fetching the heroes
+    this.messageService.add('HeroService: fetched heroes');
     // Returns an Observable
     return of(HEROES);
   }
